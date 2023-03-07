c19175d8f1dbd2d07579796573cfc517b0dae27c
徐國堂 <roberthsu2003@gmail.com>
2023-03-07 11:47:10 +0800

    modify

---
+++ 資料結構/lesson1.js
@@ -0,0 +1,10 @@
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+}
+
+r = new Rectangle(50, 30)
+console.log(r.height)
+console.log(r.width)
