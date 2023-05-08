f52ae6ce2ea7bf0c54f25de188cb43eb979e6aed
roberthsu2003 <roberthsu2003@gmail.com>
2023-05-08 10:53:20 +0800

    modify

---
+++ 2023第2期Android/c01/js/add-content.js
@@ -0,0 +1,4 @@
+var my_name = "徐國堂";
+console.log(my_name);
+var first_name = "Hsu";
+console.log(first_name);
---
+++ 2023第2期Android/c01/ts/add-content.ts
@@ -0,0 +1,4 @@
+let my_name = "徐國堂";
+console.log(my_name);
+let first_name = "Hsu";
+console.log(first_name);
