4b55ea150c72abcc7d1cf8ee2245628bea82fb30
roberthsu2003 <roberthsu2003@gmail.com>
2023-05-08 14:51:09 +0800

    modify

---
+++ 2023第2期Android/c02/js/boolean-variable.js
@@ -0,0 +1,9 @@
+let inStock;
+let shipping;
+inStock = true;
+shipping = false;
+
+let stock_span = document.getElementById('stock');
+let shipping_span = document.getElementById('shipping');
+stock_span.className = inStock.toString()
+shipping_span.className = shipping.toString()
