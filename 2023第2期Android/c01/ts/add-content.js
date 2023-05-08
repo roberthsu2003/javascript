9bdf9c9bcc4f5647f214155f4abb60107d29453f
roberthsu2003 <roberthsu2003@gmail.com>
2023-05-08 11:45:00 +0800

    modify

---
+++ 2023第2期Android/c01/ts/add-content.js
@@ -0,0 +1,17 @@
+"use strict";
+let today = new Date();
+let hourNow = today.getHours();
+let greeting;
+if (hourNow > 18) {
+    greeting = 'Good evening!';
+}
+else if (hourNow > 12) {
+    greeting = 'Good afternoon!';
+}
+else if (hourNow > 0) {
+    greeting = 'Good morning';
+}
+else {
+    greeting = 'Welcome!';
+}
+document.write('<h3>' + greeting + '</h3>');
