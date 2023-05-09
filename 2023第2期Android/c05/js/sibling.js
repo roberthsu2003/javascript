10a679c1974138fed8c11fc2711644223eeca4bc
roberthsu2003 <roberthsu2003@gmail.com>
2023-05-09 15:35:23 +0800

    modify

---
+++ 2023第2期Android/c05/js/sibling.js
@@ -0,0 +1,5 @@
+two_element = document.getElementById('two');
+var previous_element = two_element.previousElementSibling;
+var next_element = two_element.nextElementSibling;
+previous_element.className = 'complete'
+next_element.className = 'cool'
