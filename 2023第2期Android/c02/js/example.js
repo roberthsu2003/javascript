ed206f45f1cc84efe3f703a1f4f22cf2a0ff39d0
roberthsu2003 <roberthsu2003@gmail.com>
2023-05-09 11:10:43 +0800

    modify

---
+++ 2023第2期Android/c02/js/example.js
@@ -0,0 +1,6 @@
+let greeting = "Howdy";
+let my_name = "Robert";
+let message = ', please check you order:';
+let welcome = greeting +  my_name + message;
+let greetingElement = document.getElementById('greeting');
+greetingElement.innerText = welcome;
