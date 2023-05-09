e6f71a4551079bab4f2f738c2dcbf0c94c27def7
roberthsu2003 <roberthsu2003@gmail.com>
2023-05-09 14:17:47 +0800

    modify

--- 2023第2期Android/c03/js/window-object.js
+++ 2023第2期Android/c03/js/window-object.js
@@ -6,4 +6,6 @@ msg += `<p>螢幕高度:${window.screen.height}</p>`
 
 info_element = document.getElementById('info')
 info_element.innerHTML = msg;
-window.alert(`現在browser位置${window.location}`)
+//window.alert(`現在browser位置${window.location}`)
+favDialog_element = document.getElementById('favDialog')
+favDialog_element.showModal();
