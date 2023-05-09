f2d138c811a94e7000bdeec9f5c3864d301d3b2a
roberthsu2003 <roberthsu2003@gmail.com>
2023-05-09 11:36:57 +0800

    modify

--- 2023第2期Android/c02/js/example.js
+++ 2023第2期Android/c02/js/example.js
@@ -1,6 +1,28 @@
-let greeting = "Howdy";
-let my_name = "Robert";
-let message = ', please check you order:';
+let greeting = "您好";
+let my_name = "徐國堂";
+let message = ', 請檢查以下訂單:';
 let welcome = greeting +  my_name + message;
 let greetingElement = document.getElementById('greeting');
 greetingElement.innerText = welcome;
+
+let sign = "客家小館";
+let userSign_element = document.getElementById('userSign')
+userSign_element.innerText = sign;
+
+let tiles = sign.length
+let tiles_element = document.getElementById('tiles')
+tiles_element.innerText = String(tiles);
+
+let subTotal = tiles * 5;
+let subTotal_element = document.getElementById('subTotal')
+subTotal_element.innerText = '$' + String(subTotal)
+
+let shipping =  7;
+
+let shipping_element = document.getElementById('shipping')
+shipping_element.innerText = '$' + String(shipping)
+
+let grandTotal  = shipping + subTotal
+let grandTotal_element = document.getElementById('grandTotal')
+grandTotal_element.innerText = '$' + String(grandTotal)
+
