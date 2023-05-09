72c9339f4b20c9984590def64efd6776aff65e2d
roberthsu2003 <roberthsu2003@gmail.com>
2023-05-09 16:43:21 +0800

    modify

--- 2023第2期Android/c05/js/child.js
+++ 2023第2期Android/c05/js/child.js
@@ -1,7 +1,7 @@
-let ul_element = document.getElementsByTagName('ul');
+let ul_element = document.getElementsByTagName('ul')[0];
 console.log(ul_element)
-let first_child = ul_element.firstChild;
-let last_child = ul_element.lastChild;
+let first_child = ul_element.firstElementChild;
+let last_child = ul_element.lastElementChild;
 
 first_child.className = 'complete';
 last_child.className = 'cool';
