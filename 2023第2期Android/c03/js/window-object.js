54101729c0e4d5b688c59e3d3a1bcda638bd44ef
roberthsu2003 <roberthsu2003@gmail.com>
2023-05-09 14:08:46 +0800

    modify

--- 2023第2期Android/c03/js/window-object.js
+++ 2023第2期Android/c03/js/window-object.js
@@ -1,6 +1,9 @@
 let msg = '<h2>browser window</h2>'
 msg += `<p>browser_width:${window.innerWidth}</p>`
 msg += `<p>browser_height:${window.innerHeight}</p>`
+msg += `<p>螢幕寬度:${window.screen.width}</p>`
+msg += `<p>螢幕高度:${window.screen.height}</p>`
 
 info_element = document.getElementById('info')
 info_element.innerHTML = msg;
+window.alert(`現在browser位置${window.location}`)
