dac152b4a6d401f28aee3581a86b4ff2d8225017
roberthsu2003 <roberthsu2003@gmail.com>
2023-05-15 11:30:56 +0800

    modify

---
+++ 2023第2期Android/c05/js/set-attribute.js
@@ -0,0 +1,5 @@
+let oneElement = document.getElementById('one')
+oneElement.className = "complete";
+
+var fourthElement = document.getElementsByTagName('li').item(3)
+fourthElement.setAttribute('class','cool')
