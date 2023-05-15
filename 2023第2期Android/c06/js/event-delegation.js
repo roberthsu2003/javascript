9461c8b7aa065a4320cad025e589cdf11e62ac2a
roberthsu2003 <roberthsu2003@gmail.com>
2023-05-15 15:48:19 +0800

    modify

--- 2023第2期Android/c06/js/event-delegation.js
+++ 2023第2期Android/c06/js/event-delegation.js
@@ -1,6 +1,7 @@
 document.getElementById('shoppingList').addEventListener('click', function(event) {
     event.preventDefault();
     let liElement = event.target.parentNode;    
-    let ulElement = event.currentTarget;
-    ulElement.removeChild(liElement);
+    //let ulElement = event.currentTarget;
+    //ulElement.removeChild(liElement);
+    liElement.remove();
 });
