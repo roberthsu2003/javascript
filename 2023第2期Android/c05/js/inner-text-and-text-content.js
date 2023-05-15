fec55bee331e8a01e67af36fc384e79cd29f23b8
roberthsu2003 <roberthsu2003@gmail.com>
2023-05-15 09:51:30 +0800

    modify

---
+++ 2023第2期Android/c05/js/inner-text-and-text-content.js
@@ -0,0 +1,4 @@
+let ElementOne = document.getElementById('one')
+//let innerHTML_string = ElementOne.innerHTML
+let innerText_string = ElementOne.innerText
+let textContent_string = ElementOne.textContent
