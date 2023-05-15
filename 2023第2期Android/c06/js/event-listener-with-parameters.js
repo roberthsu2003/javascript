cea307e8b4dc2497433a62416b3d797751ee453b
roberthsu2003 <roberthsu2003@gmail.com>
2023-05-15 14:44:51 +0800

    modify

--- 2023第2期Android/c06/js/event-listener-with-parameters.js
+++ 2023第2期Android/c06/js/event-listener-with-parameters.js
@@ -17,8 +17,9 @@ document.getElementById('username').addEventListener('blur', checkUsername, fals
 
 document.getElementById('username').addEventListener('blur', function(event){
     let feedbackElement = document.getElementById('feedback')
-    let username = event.target.value
-    if(username.length < 5){
+    let username_string = event.target.value
+    console.log(username)
+    if(username_string.length < 5){
         feedbackElement.textContent = "使用者的名綱必需大新5個字元"
     }else{
         feedbackElement.textContent =  ""
