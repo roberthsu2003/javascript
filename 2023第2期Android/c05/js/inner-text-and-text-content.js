3fe160167d3bbbfcb5d5e1788200b711d9931c4d
roberthsu2003 <roberthsu2003@gmail.com>
2023-05-15 10:03:16 +0800

    modify

--- 2023第2期Android/c05/js/inner-text-and-text-content.js
+++ 2023第2期Android/c05/js/inner-text-and-text-content.js
@@ -2,3 +2,10 @@ let ElementOne = document.getElementById('one')
 //let innerHTML_string = ElementOne.innerHTML
 let innerText_string = ElementOne.innerText
 let textContent_string = ElementOne.textContent
+
+let msg = '<p>textContent:' + textContent_string +'</p>'
+msg += '<p>innerText:' + innerText_string + '</p>'
+
+let elementResults = document.getElementById('scriptResults')
+elementResults.innerHTML = msg
+ElementOne.textContent  = '被取代了'
