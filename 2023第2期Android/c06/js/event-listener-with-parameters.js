67f23ea9d979cfdd08ce45ac78486f067b084235
roberthsu2003 <roberthsu2003@gmail.com>
2023-05-15 14:39:24 +0800

    modify

---
+++ 2023第2期Android/c06/js/event-listener-with-parameters.js
@@ -0,0 +1,26 @@
+//event listener with parameters
+// document.getElementById('username').addEventListener('keyup', function(event){
+//     console.log(event.target.value)
+// }
+/*
+function checkUsername(event){
+    let feedbackElement = document.getElementById('feedback')    
+    let username = event.target.value
+    if(username.length < 5){
+        feedbackElement.textContent = "使用者的名稱必需大於5個字元"
+    }else{
+        feedbackElement.textContent =  ""
+    }
+}
+document.getElementById('username').addEventListener('blur', checkUsername, false);
+*/
+
+document.getElementById('username').addEventListener('blur', function(event){
+    let feedbackElement = document.getElementById('feedback')
+    let username = event.target.value
+    if(username.length < 5){
+        feedbackElement.textContent = "使用者的名綱必需大新5個字元"
+    }else{
+        feedbackElement.textContent =  ""
+    }
+}, false);
