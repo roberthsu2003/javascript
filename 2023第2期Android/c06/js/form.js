55185041f69616be36266fb0608aa6b048176880
roberthsu2003 <roberthsu2003@gmail.com>
2023-05-16 10:54:43 +0800

    modify

--- 2023第2期Android/c06/js/form.js
+++ 2023第2期Android/c06/js/form.js
@@ -1,5 +1,9 @@
 let formElement = document.getElementById('formSignup')
 let packageElement = document.getElementById('package')
+let packageHintElement = document.getElementById('packageHint')
+let termsElement = document.getElementById('terms')
+let termsHintElement = document.getElementById('termsHint')
+
 
 formElement.addEventListener('submit', (event) => {
     console.log('submitting')
@@ -7,5 +11,17 @@ formElement.addEventListener('submit', (event) => {
 })
 
 packageElement.addEventListener('change', (event) => {
-    console.log('changing')    
-})
+    console.log('changing')
+    /*
+    for(const option of packageElement.options){
+        console.log(option.label)
+        console.log(option.value)
+    }*/
+    let selectedIndex = packageElement.selectedIndex
+    if(packageElement.options[selectedIndex].value=='monthly'){
+        packageHintElement.innerText = '如果選擇1年,可以省10元美金'
+    }else{
+        packageHintElement.innerText = '正確的選擇'
+    }
+}
+)
