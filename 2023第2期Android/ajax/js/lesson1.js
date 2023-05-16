a4c01b793fea76b82ab572b3081f3d92ed307f94
roberthsu2003 <roberthsu2003@gmail.com>
2023-05-16 14:53:20 +0800

    modify

--- 2023第2期Android/ajax/js/lesson1.js
+++ 2023第2期Android/ajax/js/lesson1.js
@@ -6,7 +6,8 @@ const windowload = (event) => {
     console.log('網頁已經全部被載入');    
     const req = new XMLHttpRequest();
     req.addEventListener("load", reqListener);
-    req.open("GET", "http://www.example.org/example.txt");
+req.open("GET", "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json");
     req.send();
 }
+
 window.addEventListener('load', windowload)
