2722a0e58a2ade5e90896e04dd4ea50f8dada191
roberthsu2003 <roberthsu2003@gmail.com>
2023-05-16 11:29:30 +0800

    modify

--- 2023第2期Android/c06/js/mutation.js
+++ 2023第2期Android/c06/js/mutation.js
@@ -1,6 +1,11 @@
 let addElement = document.querySelector('a.add')
+let ulElement = document.querySelector('#list')
+
 addElement.addEventListener('click', (event)=>{
     console.log('click')    
     event.preventDefault()
-    
+    let liElement = document.createElement('li')
+    let textnode = document.createTextNode("New Item")
+    liElement.appendChild(textnode)
+    ulElement.appendChild(liElement)    
 })
