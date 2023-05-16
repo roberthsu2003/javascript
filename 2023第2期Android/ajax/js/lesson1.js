3576509e8e65867b6f94cabd8cc88d54cb0343b7
roberthsu2003 <roberthsu2003@gmail.com>
2023-05-16 14:39:22 +0800

    modify

--- 2023第2期Android/ajax/js/lesson1.js
+++ 2023第2期Android/ajax/js/lesson1.js
@@ -5,7 +5,6 @@ function reqListener() {
 const windowload = (event) => {
     console.log('網頁已經全部被載入');    
     const req = new XMLHttpRequest();
-    req .onreadystatechange = handleStateChange;
     req.addEventListener("load", reqListener);
     req.open("GET", "http://www.example.org/example.txt");
     req.send();
