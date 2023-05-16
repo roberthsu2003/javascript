7d1c9b3684ec4948187f74aba212389eb4aeed5f
roberthsu2003 <roberthsu2003@gmail.com>
2023-05-16 14:25:41 +0800

    modify

--- 2023第2期Android/ajax/js/lesson1.js
+++ 2023第2期Android/ajax/js/lesson1.js
@@ -1,4 +1,13 @@
+function reqListener() {
+    console.log(this.responseText);
+}
+
 const windowload = (event) => {
-    console.log('網頁已經全部被載入');
+    console.log('網頁已經全部被載入');    
+    const req = new XMLHttpRequest();
+    req .onreadystatechange = handleStateChange;
+    req.addEventListener("load", reqListener);
+    req.open("GET", "http://www.example.org/example.txt");
+    req.send();
 }
 window.addEventListener('load', windowload)
