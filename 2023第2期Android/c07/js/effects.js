18899ace4174cf1865a3a1078db88aeb6a192c71
roberthsu2003 <roberthsu2003@gmail.com>
2023-05-23 11:27:18 +0800

    modify

--- 2023第2期Android/c07/js/effects.js
+++ 2023第2期Android/c07/js/effects.js
@@ -1,2 +1,6 @@
 let h2Element = document.querySelector('h2')
 $(h2Element).hide().slideDown()
+let liElements = document.querySelectorAll('li')
+liElements.forEach((element)=>{
+    $(element).hide().delay(700).fadeIn(700)
+})
