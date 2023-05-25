3b7432a84516a78fc8ddb271833831f55052b9cf
roberthsu2003 <roberthsu2003@gmail.com>
2023-05-25 16:40:58 +0800

    modify

--- 2023第2期Android/product/public/js/index.js
+++ 2023第2期Android/product/public/js/index.js
@@ -14,10 +14,28 @@ const validateName = ()=>{
     allOfDataArray.push({'productName':productName})
 }
 
+const validateCodeFormat = () =>{
+    let inputCodeElement = document.querySelector('#inputCode')
+    let codeAlertElement = document.querySelector('#codeAlert')
+    let inputCodeValue = inputCodeElement.value
+    console.log(inputCodeValue)
+    if (inputCodeValue.length == 0){
+        //console.log("沒有填資料")
+        codeAlertElement.classList.remove("close")
+        return
+    }
+    allOfDataArray.push({'code':inputCodeValue})
+}
+
 const clearAllAlertAndData = ()=>{
     //清除產品警告
     let nameAlertElement = document.querySelector('#nameAlert')
     nameAlertElement.classList.add("close")
+
+    //清除code警告
+    let codeAlertElement = document.querySelector('#codeAlert')
+    codeAlertElement.classList.add("close")
+
     //清除收集的資料
     allOfDataArray = []
 }
@@ -26,5 +44,6 @@ formElement.addEventListener('submit',(event)=>{
     clearAllAlertAndData()
     event.preventDefault()
     validateName()
+    validateCodeFormat()
     console.log(allOfDataArray)
 })
