7ef74eba622af8d0bc221082159d85d3a1c2ba98
roberthsu2003 <roberthsu2003@gmail.com>
2023-05-29 14:18:01 +0800

    modify

--- 2023第2期Android/product/public/js/index.js
+++ 2023第2期Android/product/public/js/index.js
@@ -1,3 +1,18 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js'
+
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC0AO8-o_0FoVZgLfVs31UvYm1Gm-fL6lI",
+    authDomain: "project-javascript-4e903.firebaseapp.com",
+    databaseURL: "https://project-javascript-4e903-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "project-javascript-4e903",
+    storageBucket: "project-javascript-4e903.appspot.com",
+    messagingSenderId: "785523512113",
+    appId: "1:785523512113:web:675e257b7b759e00b6d610"
+};
+const app = initializeApp(firebaseConfig);
+
 let formElement = document.querySelector('#form')
 let allOfDataArray = [];
 
